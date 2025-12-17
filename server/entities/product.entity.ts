c258f549377225a7d7be8061e84df9b9198b61ae
Henrique <henrique@sidekick-coder.com>
2025-12-17 18:48:57 -0300

    feat: product metas entity link

--- server/entities/product.entity.ts
+++ server/entities/product.entity.ts
@@ -1,8 +1,17 @@
 import { Model } from '#server/mixins/model.mixin.ts'
 import Base from '#zpayments/shared/entities/product.entity.ts'
 import { composeWith } from '#shared/utils/compose.ts'
+import MetadataService from '#server/services/metadata.service.ts'
 
 export default class Product extends composeWith(
     Base,
     Model('zpayments__products')
-) {}
+) {
+    public get $metas(){
+        return new MetadataService({
+            foreignKey: 'product_id',
+            table: 'zpayments__product_metas',
+            id: this.id,
+        })
+    }
+}
